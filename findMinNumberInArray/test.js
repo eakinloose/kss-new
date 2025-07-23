92f65d64e8c772fe35762bf2b4b276f19f142a51
Adamz <theophilusinaede@gmail.com>
2025-07-23 14:12:54 +0100

    Na men dey run am

---
+++ findMinNumberInArray/test.js
@@ -0,0 +1,15 @@
+const findMinNumberInArray = require("./findMinNumberInArray");
+
+test("check minimum numebr from array of negative numbers", () => {
+    expect(
+      findMinNumberInArray([
+        -3, -4, -5, -8, -7, -6, -9, -23, -23.5, -23.51, -4, -6, -7, -8, -9,
+      ])
+    ).toBe(-23.51);
+    expect(
+      findMinNumberInArray([
+        -3, -4, -5, -8, -7, -6, -9, -23, -23.5, -4, -6, -7, -8, -9,
+      ])
+    ).toBe(-23.5);
+  });
+  
